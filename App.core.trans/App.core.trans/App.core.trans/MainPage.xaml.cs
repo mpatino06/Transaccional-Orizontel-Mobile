db45018908d215af0e49b036eae390251347f711
Miguel Angel Patiño <mpatino06@hotmail.com>
2018-09-10 23:53:24 -0500

    Menu
    Proyecto inicial

---
+++ App.core.trans/App.core.trans/App.core.trans/MainPage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App.core.trans.Models;
+using App.core.trans.Views;
+using App.core.trans.Views.Count;
+using Xamarin.Forms;
+
+namespace App.core.trans
+{
+	public partial class MainPage : MasterDetailPage
+	{
+		public List<MasterPageItem> menuList { get; set; }
+
+		public MainPage()
+		{
+			InitializeComponent();
+			menuList = new List<MasterPageItem>();
+			LoadMenu();
+		}
+
+		public void LoadMenu()
+		{
+			menuList.Add(new MasterPageItem
+			{
+				Title = "Plan de Conteo",
+				Icon = "ic1.png",
+				TargetType = typeof(PlanList)
+			});
+			menuList.Add(new MasterPageItem
+			{
+				Title = "Recepcion",
+				Icon = "ic1.png",
+				TargetType = typeof(PlanList)
+			});
+			menuList.Add(new MasterPageItem
+			{
+				Title = "Tansferencia entre Bodegas",
+				Icon = "ic1.png",
+				TargetType = typeof(PlanList)
+			});
+
+
+			navigationDrawerList.ItemsSource = (IEnumerable)this.menuList;
+			Detail = (Page)new NavigationPage((Page)Activator.CreateInstance(typeof(MainMenuDetail)))
+			{
+				BarBackgroundColor = Color.FromHex("#082adb"),
+				BarTextColor = Color.White
+			};
+		}
+
+		private void OnMenuItemSelected(object sender, SelectedItemChangedEventArgs e)
+		{
+			MasterPageItem selectedItem = e.SelectedItem as MasterPageItem;
+			if (selectedItem == null)
+				return;
+			Detail = (Page)new NavigationPage((Page)Activator.CreateInstance(selectedItem.TargetType))
+			{
+				BarBackgroundColor = Color.FromHex("#082adb"),
+				BarTextColor = Color.White
+			};
+			navigationDrawerList.SelectedItem = (object)null;
+			IsPresented = false;
+		}
+
+
+	}
+}
+
---
+++ App.core.trans/App.core.trans/App.core.trans/Models/MasterPageItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.core.trans.Models
+{
+	public class MasterPageItem
+	{
+		public string Title { get; set; }
+
+		public string Icon { get; set; }
+
+		public Type TargetType { get; set; }
+	}
+}
+
